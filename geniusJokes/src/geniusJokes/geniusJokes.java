313e84bafff65b4159921b358d57e4a94fb9b46f
HGP44 <hgp44@thes-air-6.attlocal.net>
2021-09-22 18:10:42 -0700

    Commision

---
+++ geniusJokes/src/geniusJokes/geniusJokes.java
@@ -0,0 +1,12 @@
+package geniusJokes;
+
+public class geniusJokes {
+	public static void main(String[] args) {
+	    System.out.println("Hello! My name is Quincy Braxton"); //Print My Welcome Message & Name
+	    System.out.println("What did the ninja say to his friend after his friend asked for a favor? "); //Print Joke's Setup
+	    System.out.println("Sure-i-can!");
+	    
+	  }
+	}
+
+
